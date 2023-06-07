c20f1a380fab787b183f2650b50bc432bca47f27
Niraj Kumar <nirajk23523@gmail.com>
2023-06-08 00:25:34 +0530

    added images and used view instead of SafeAreaView

--- app/index.js
+++ app/index.js
@@ -1,14 +1,17 @@
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, Image, ImageBackground } from "react-native";
+// import { SafeAreaView } from "react-native-safe-area-context";
 
 import { COLORS, SIZES } from "../constants/theme";
 import { Stack } from "expo-router";
 import ScreenHeaderBtn from "../components/common/header/ScreenHeaderBtn";
+import Announcements from "../components/home/Announcements";
 import menuIcon from "../assets/icons/menu.png";
+import logo from "../assets/images/iittnifLogo.png";
+import imageBg from "../assets/images/satelliteBg.jpeg";
 
 const Home = () => {
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: "pink" }}>
+        <View style={{ flex: 1, backgroundColor: COLORS.pureWhite }}>
             <Stack.Screen
                 options={{
                     headerTitle: "Home",
@@ -18,24 +21,65 @@ const Home = () => {
                         fontSize: 20,
                     },
                     headerStyle: { backgroundColor: COLORS.pureWhite },
-                    headerShadowVisible: false,
+                    // headerShadowVisible: false,
                     headerLeft: () => (
                         <ScreenHeaderBtn iconUrl={menuIcon} dimension="60%" />
                     ),
                 }}
             />
+
             <ScrollView>
                 <View
                     style={{
                         flex: 1,
-                        padding: SIZES.medium,
-                        backgroundColor: "red",
+                        padding: SIZES.small,
+                        paddingTop: 0,
                     }}
                 >
-                    <Text>Home</Text>
+                    <View
+                        style={{
+                            width: "100%",
+                            height: 150,
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Image
+                            source={logo}
+                            resizeMode="contain"
+                            style={{ width: "100%", height: "100%" }}
+                        />
+                    </View>
+                    <View
+                        style={{
+                            width: "100%",
+                        }}
+                    >
+                        <ImageBackground
+                            source={imageBg}
+                            style={{
+                                height: 200,
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Text
+                                style={{
+                                    color: COLORS.pureWhite,
+                                    fontFamily: "NunitoMedium",
+                                    fontSize: SIZES.xLarge,
+                                    textAlign: "center",
+                                }}
+                            >
+                                Technology Innovation Hub in Positioning and
+                                Precision Technologies
+                            </Text>
+                        </ImageBackground>
+                    </View>
                 </View>
+                <Announcements />
             </ScrollView>
-        </SafeAreaView>
+        </View>
     );
 };
 
