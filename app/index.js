3acb153d567e67745df82754a41ab7899a16b787
Niraj Kumar <nirajk23523@gmail.com>
2023-06-07 23:37:38 +0530

    add menu icon with scope for further customization

--- app/index.js
+++ app/index.js
@@ -1,13 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { COLORS } from "../constants/theme";
+import { COLORS, SIZES } from "../constants/theme";
 import { Stack } from "expo-router";
-import { Ionicons } from "@expo/vector-icons";
+import ScreenHeaderBtn from "../components/common/header/ScreenHeaderBtn";
+import menuIcon from "../assets/icons/menu.png";
 
 const Home = () => {
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "pink" }}>
             <Stack.Screen
                 options={{
                     headerTitle: "Home",
@@ -18,9 +19,22 @@ const Home = () => {
                     },
                     headerStyle: { backgroundColor: COLORS.pureWhite },
                     headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={menuIcon} dimension="60%" />
+                    ),
                 }}
             />
-            <Text>Hello, world</Text>
+            <ScrollView>
+                <View
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium,
+                        backgroundColor: "red",
+                    }}
+                >
+                    <Text>Home</Text>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 };
