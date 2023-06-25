5f5d063b8424067bc8239acfa477d46e7f0a76da
Niraj Kumar <nirajk23523@gmail.com>
2023-06-26 00:21:50 +0530

    added educationalDetails validation schema

--- app/student/signup.js
+++ app/student/signup.js
@@ -7,6 +7,21 @@ import { Stack } from "expo-router";
 import { COLORS } from "../../constants/theme";
 
 const Signup = () => {
+    const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
     const studentSignupSchema = Yup.object().shape({
         email: Yup.string()
             .email("Invalid email")
@@ -27,6 +42,54 @@ const Signup = () => {
         country: Yup.string(),
         currentJobTitle: Yup.string(),
         skillSet: Yup.string(),
+        educationalDetails: Yup.array().of(
+            Yup.object()
+                .shape({
+                    instituteOrSchool: Yup.string().required("Required"),
+                    majorOrDepartment: Yup.string().required("Required"),
+                    degree: Yup.string().required("Required"),
+                    startMonth: Yup.string()
+                        .oneOf(monthNames, "Invalid start month")
+                        .required("Start month is required"),
+                    startYear: Yup.number().required("Start year is required"),
+                    endMonth: Yup.string()
+                        .oneOf(monthNames, "Invalid end month")
+                        .required("End month is required"),
+                    endYear: Yup.number().required("End year is required"),
+                    currentlyPursuing: Yup.boolean(),
+                })
+                .test(
+                    "validDateRange",
+                    "Invalid date range",
+                    function (values) {
+                        const { startMonth, startYear, endMonth, endYear } =
+                            values;
+                        const startMonthIndex = monthNames.indexOf(startMonth);
+                        const endMonthIndex = monthNames.indexOf(endMonth);
+
+                        if (startYear > endYear) {
+                            return this.createError({
+                                path: "endYear",
+                                message:
+                                    "End year should be greater than start year",
+                            });
+                        }
+
+                        if (
+                            startYear === endYear &&
+                            startMonthIndex > endMonthIndex
+                        ) {
+                            return this.createError({
+                                path: "endMonth",
+                                message:
+                                    "End month should be greater than start month",
+                            });
+                        }
+
+                        return true;
+                    }
+                )
+        ),
     });
 
     const {
@@ -54,6 +117,7 @@ const Signup = () => {
             country: "",
             currentJobTitle: "",
             skillSet: "",
+            educationalDetails: [],
         },
         onSubmit: (values) => {
             alert(JSON.stringify(values, null, 2));
